a9c732d1091e4b99107587e5088276955c56ba27
Heather Nuffer <zzzuzz@gmail.com>
2019-11-11 11:51:03 -0700

    Added styling to event types in pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import coupleDancing from '../images/coupleDancing.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, 
+import { faHeart,
+    faMusic,
+    faSchool,
+    faBirthdayCake 
     } from '@fortawesome/free-solid-svg-icons';
 
 const Pricing = (props) => {
@@ -14,37 +17,54 @@ const Pricing = (props) => {
             <div className="img-container" >
                 <img src={coupleDancing} alt="Bride and Groom Dancing" />
             </div>
-            <FontAwesomeIcon icon={faHeart} />
-            <h2>Wedding*</h2>
 
-                <p>$495.00 - up to 5 hours</p>
-                <p>Includes everything: ceremony, reception, background music, dancing, announcements, and unlimited fun.
-                </p>
+            <div className="event-types" >
+                <container className="event-type">
+                <FontAwesomeIcon icon={faHeart} />
+                    <h2>Wedding*</h2>
 
-            <h2>Company Event*</h2>
+                        <p>$495.00 - up to 5 hours</p>
+                        <p>Includes everything:<br /> ceremony, reception, background music, dancing, announcements, and unlimited fun.
+                        </p>
+                </container>
 
-                <p>$395.00 - up to 5 hours</p>
-                <p>Includes everything: dinner music, dancing, games, announcements, and much more.
-                </p>
+                <container className="event-type">
+                    <FontAwesomeIcon icon={faMusic} />
+                    <h2>Company Event*</h2>
 
-            <h2>School Dance*</h2>
+                        <p>$395.00 - up to 5 hours</p>
+                        <p>Includes everything:<br /> dinner music, dancing, games, announcements, and much more.
+                        </p>
+                </container>
 
-                <p>$295.00 - up to 5 hours</p>
-                <p>Includes everything: dancing, games, contests, awards, announcements, and an unforgettable evening.
-                </p>
+                <container className="event-type">
 
-            <h2> Other Events</h2>
-                <p>Other events not listed (bar/bat mitzvahs, community events, church dances, reunions, anniversaries, shows, conventions, festivals, races, and many others) please contact DJ.DJ Entertainment for pricing.
-                </p>
-            
+                    <FontAwesomeIcon icon={faSchool} />
+                    <h2>School Dance*</h2>
+                    <p>$295.00 - up to 5 hours</p>
+                    <p>Includes everything:<br /> dancing, games, contests, awards, announcements, and an unforgettable evening.
+                    </p>
+                </container>
+
+                <container className="event-type">
+                    <FontAwesomeIcon icon={faBirthdayCake} />
+                    <h2> Other Events</h2>
+                        <p>Other events not listed (bar/bat mitzvahs, community events, 
+                            church dances, reunions, anniversaries, shows, conventions, 
+                            festivals, races, and many others) please contact DJ.DJ 
+                            Entertainment for pricing.
+                        </p>
+                </container>
+            </div>
             <hr />
-            <p>
-            *All prices listed are standard estimates only, 
-            prices may be more or less in some circumstances: 
-            travel over 150 miles, day of the week, & hours of work. 
-            For an exact quote on your event please call or e-mail. 
+            <p className="note">
+            *All prices listed are standard estimates only. 
+            Prices may be more or less in some circumstances: 
+            travel over 150 miles, day of the week, & hours of work.
+            For an exact quote on your event, please call or e-mail.</p>
+            <p className="note" >  
             For all events, a $100 deposit is required to secure services 
-            for your date requested, this will be subtracted from your total.
+            for your date requested; this will be subtracted from your total.
             </p>
         </div>
     );
